76175708c846be8ad44bfd3ff1ab950e6735b537
Dylan Nguyen <spaceygt@gmail.com>
2026-08-23 19:00:51 -0500

    Added Discord file uploads. Expanded role functions and created private channels.

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.guilds = True
 
 bot = commands.Bot(command_prefix='/', intents=intents, help_command=None)
 
@@ -28,30 +29,67 @@ async def on_member_join(member):
 @bot.command()
 # to change the name of command, change right before (ctx):
 async def hello(ctx):
-    await ctx.send(f"Hello {ctx.author.mention}!")
+    await ctx.send(f"Hello {ctx.author.mention}!", delete_after=4.0)
 
-@bot.command()
+@bot.command(hidden=True) #hidden=True hides command from /command
 async def assignrole(ctx):
-    role = discord.utils.get(ctx.guild.roles, name=newrole)
-    if role:
-        await ctx.author.add_roles(role)
-        await ctx.send(f"{ctx.author.mention} is now my {newrole}")
-    else:
-        await ctx.send("Role doesn't exist")
+    await ctx.message.delete()
 
+    guild = ctx.guild
+    user = ctx.author
 
-@bot.command()
+    role = discord.utils.get(guild.roles, name=newrole)
+
+    if not role:
+        await ctx.message.delete(delay=4.0)
+        await ctx.send("role doesn't exist", delete_after=4.0)
+        return
+
+    if role in user.roles:
+        await ctx.message.delete(delay=4.0)
+        await ctx.send(f"{user.mention}, you already have the **{newrole}** role", delete_after=4.0)
+        return
+    
+    await ctx.author.add_roles(role)
+
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(view_channel=False),
+
+        user: discord.PermissionOverwrite(view_channel=True, send_messages=True),
+
+        guild.me: discord.PermissionOverwrite(view_channel=True, send_messages=True)
+    }
+
+    channel_name = f"private-{user.name}".replace(" ", "-").lower()
+    private_channel = await guild.create_text_channel(name=channel_name, overwrites=overwrites)
+
+    await private_channel.send(f"Welcome to your private room, {user.mention}!")
+
+    await ctx.send(f"{ctx.author.mention} is now my {newrole}", delete_after=4.0)
+
+@bot.command(hidden=True)
 async def commands(ctx):
     command_list = []
     for cmd in bot.commands:
+        if cmd.hidden:
+            continue
+
         command_list.append(f"'/{cmd.name}'")
 
     final_string = ", ".join(command_list)
 
-    await ctx.send(f"Here are the commands! {final_string}")
+    await ctx.send(f"Here are the commands! {final_string}", delete_after=6.0)
+
+    await ctx.message.delete(delay=4.0)
 
 @bot.command()
 async def syllabus(ctx, attachment: discord.Attachment):
-    await ctx.send(f"I got your file: {attachment.filename}")
+    file_path = os.path.join("temp_uploads", attachment.filename)
+
+    await attachment.save(file_path)
+
+    await ctx.author.send(f"Saved {attachment.filename}!")
+
+    await ctx.message.delete(delay=4.0)
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
